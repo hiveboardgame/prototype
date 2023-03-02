3fc9fc854bfcc5f7ae0bef84d9273d51a71d3676
Will Greenberg <willg@eff.org>
2023-03-02 00:48:38 -0800

    backend: basic validation on user uid/username

--- backend/src/model/user.rs
+++ backend/src/model/user.rs
@@ -5,18 +5,53 @@ use diesel::{result::Error, Identifiable, Insertable, QueryDsl, Queryable};
 use diesel_async::RunQueryDsl;
 use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Identifiable, Insertable, Serialize, Deserialize)]
-#[primary_key(uid)]
+const MAX_USERNAME_LENGTH: usize = 20;
+const VALID_USERNAME_CHARS: &str = "-_";
+
+#[derive(Debug)]
+pub enum UserCreationError {
+    InvalidUid(String),
+    InvalidUsername(String),
+}
+
+fn validate_uid(uid: &str) -> Result<(), UserCreationError> {
+    if !uid.chars().all(|c| c.is_ascii_alphanumeric()) {
+        return Err(UserCreationError::InvalidUid(format!("invalid uid: {:?}", uid)));
+    }
+    Ok(())
+}
+
+fn validate_username(username: &str) -> Result<(), UserCreationError> {
+    if !username.chars().all(|c| c.is_ascii_alphanumeric() || VALID_USERNAME_CHARS.contains(c)) {
+        return Err(UserCreationError::InvalidUsername(format!("invalid username characters: {:?}", username)));
+    } else if username.len() > MAX_USERNAME_LENGTH {
+        return Err(UserCreationError::InvalidUsername(format!("username must be <= {} chars", MAX_USERNAME_LENGTH)));
+    }
+    Ok(())
+}
+
+#[derive(Queryable, Identifiable, Insertable, Serialize, Deserialize, Debug)]
+#[diesel(primary_key(uid))]
 pub struct User {
-    pub uid: String,
-    pub username: String,
+    uid: String,
+    username: String,
     pub is_guest: bool,
 }
 
 impl User {
+    pub fn new(uid: &str, username: &str, is_guest: bool) -> Result<User, UserCreationError> {
+        validate_uid(uid)?;
+        validate_username(username)?;
+        Ok(User {
+            uid: uid.into(),
+            username: username.into(),
+            is_guest,
+        })
+    }
+
     pub async fn find_by_uid(pool: &DbPool, uid: &str) -> Result<User, Error> {
         let conn = &mut get_conn(pool).await?;
-        users_table.find(uid).first::<Self>(conn).await
+        users_table.find(uid).first(conn).await
     }
 
     pub async fn insert(&self, pool: &DbPool) -> Result<(), Error> {
