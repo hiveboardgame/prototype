b5f8603ffe3e8ece0efd8d9179f91ebff0299b8b
Will Greenberg <willg@eff.org>
2023-03-12 13:34:14 -0700

    frontend: nextjs dev server proxies /api requests to the rust backend

--- js/frontend/next.config.js
+++ js/frontend/next.config.js
@@ -1,5 +1,13 @@
 const withTM = require('next-transpile-modules')(['hive-db']);
 
 module.exports = withTM({
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8080/api/:path*',
+      }
+    ]
+  },
   reactStrictMode: true,
 });
