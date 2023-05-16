4801361acb80c8808c31b75cd845945e72b131fa
ioni <104294646+IongIer@users.noreply.github.com>
2023-05-16 16:52:55 +0300

    Test playing out a game paired with @klautcomputing

--- backend/src/api/game/play.rs
+++ backend/src/api/game/play.rs
@@ -407,4 +407,125 @@ mod tests {
             &(game.turn as i32, GameControl::Abort(Color::Black))
         );
     }
+    #[test_context(DBTest)]
+    #[actix_rt::test]
+    #[serial]
+    async fn test_takeback_move(_ctx: &mut DBTest) {
+        let app = test::init_service(crate::new_test_app().await).await;
+        let black = make_user!("black", &app);
+        let white = make_user!("white", &app);
+        let challenge_response = make_challenge!(white.uid.clone(), "White", &app);
+        let game = accept_challenge!(challenge_response.id, black.uid.clone(), &app);
+        let game = play_turn!(game.game_id, white.uid.clone(), ["wL", "."], &app);
+        assert_eq!(game.turn, 1);
+        assert_eq!(game.history, vec![("wL".to_string(), ".".to_string())]);
+        let game = play_turn!(game.game_id, black.uid.clone(), ["bL", "wL-"], &app);
+        assert_eq!(game.turn, 2);
+        let game = game_control!(
+            game.game_id,
+            white.uid.clone(),
+            "TakebackRequest",
+            "White",
+            &app
+        );
+        assert_eq!(
+            game.game_control_history.last().unwrap(),
+            &(game.turn as i32, GameControl::TakebackRequest(Color::White))
+        );
+        let game = game_control!(
+            game.game_id,
+            black.uid.clone(),
+            "TakebackReject",
+            "Black",
+            &app
+        );
+        assert_eq!(
+            game.game_control_history.last().unwrap(),
+            &(game.turn as i32, GameControl::TakebackReject(Color::Black))
+        );
+        let game = play_turn!(game.game_id, white.uid.clone(), ["wQ", "-wL"], &app);
+        let game = game_control!(
+            game.game_id,
+            black.uid.clone(),
+            "TakebackRequest",
+            "Black",
+            &app
+        );
+        let game = game_control!(
+            game.game_id,
+            white.uid.clone(),
+            "TakebackAccept",
+            "White",
+            &app
+        );
+        assert_eq!(game.turn, 2);
+        assert_eq!(
+            game.history,
+            vec![
+                ("wL".to_string(), ".".to_string()),
+                ("bL".to_string(), "wL-".to_string())
+            ]
+        );
+        assert_eq!(
+            game.game_control_history.last().unwrap(),
+            &(
+                (game.turn + 1) as i32,
+                GameControl::TakebackAccept(Color::White)
+            )
+        );
+    }
+    #[test_context(DBTest)]
+    #[actix_rt::test]
+    #[serial]
+    async fn test_play_game(_ctx: &mut DBTest) {
+        let app = test::init_service(crate::new_test_app().await).await;
+        let black = make_user!("black", &app);
+        let white = make_user!("white", &app);
+        let challenge_response = make_challenge!(white.uid.clone(), "White", &app);
+        let mut game = accept_challenge!(challenge_response.id, black.uid.clone(), &app);
+        let moves = vec![
+            ["wL", "."],
+            ["bL", "/wL"],
+            ["wM", "wL/"],
+            ["bQ", "bL\\"],
+            ["wQ", "\\wL"],
+            ["bP", "bQ\\"],
+            ["wA1", "wM\\"],
+            ["bA1", "-bL"],
+            ["wM", "/bQ"],
+            ["bA1", "\\wQ"],
+            ["wB1", "/wM"],
+            ["bB1", "bQ-"],
+            ["wA1", "bB1-"],
+            ["bB2", "bP\\"],
+            ["wB1", "wM"],
+            ["bA1", "wL\\"],
+            ["wB1", "-bQ"],
+        ];
+        for (i, mov) in moves.iter().enumerate() {
+            if i % 2 == 0 {
+                game = play_turn!(game.game_id, white.uid.clone(), mov, &app);
+            } else {
+                game = play_turn!(game.game_id, black.uid.clone(), mov, &app);
+            }
+        }
+        assert_eq!(
+            game.game_status,
+            hive_lib::game_status::GameStatus::Finished(hive_lib::game_result::GameResult::Winner(
+                hive_lib::color::Color::White
+            ))
+        );
+        assert_eq!(game.turn, 16);
+        // Can't play on a finished game
+        let request_body = json!({
+            "Turn": ["bL", "-wQ"],
+        });
+        let resp = TestRequest::post()
+            .uri(&format!("/api/game/{}/play", game.game_id))
+            .set_json(&request_body)
+            .insert_header(("x-authentication", "black"))
+            .send_request(&app)
+            .await;
+        assert!(resp.status().is_client_error());
+    }
 }
