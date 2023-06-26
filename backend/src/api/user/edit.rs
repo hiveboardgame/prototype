c822c5639b95838701184efba413e679e732dd78
ioni <104294646+IongIer@users.noreply.github.com>
2023-06-26 12:10:26 +0300

    Edit user is now a PUT to /api/user/

--- backend/src/api/user/edit.rs
+++ backend/src/api/user/edit.rs
@@ -4,7 +4,7 @@ use crate::model::user::User;
 use crate::server_error::ServerError;
 use crate::user::user_response::UserResponse;
 use actix_web::{
-    http, post,
+    http, put,
     web::{self, Json},
 };
 use serde::Deserialize;
@@ -14,14 +14,19 @@ pub struct EditedUserBody {
     username: String,
 }
 
-#[post("/user/edit")]
+#[put("/user/")]
 pub async fn edit_user(
     auth_user: AuthenticatedUser,
     edited_user: web::Json<EditedUserBody>,
     pool: web::Data<DbPool>,
 ) -> Result<(Json<UserResponse>, http::StatusCode), ServerError> {
     let user = User::find_by_uid(&auth_user.uid, &pool).await?;
-    user.update_username(&edited_user.username, &pool).await?;
+    user.update_username(&edited_user.username, &pool)
+        .await
+        .map_err(|_| ServerError::UserInputError {
+            field: "username".to_string(),
+            reason: "already exists".to_string(),
+        })?;
     let user_response = UserResponse::from_uid(&auth_user.uid, &pool).await?;
     Ok((Json(user_response), http::StatusCode::CREATED))
 }
@@ -41,24 +46,24 @@ mod tests {
     #[serial]
     async fn edit_user(_ctx: &mut DBTest) {
         let app = test::init_service(crate::new_test_app().await).await;
-        let _user = make_user!("black", &app);
+        let _black = make_user!("black", &app);
         let _white = make_user!("white", &app);
         let request_body = json!({
             "username": "white",
         });
-        let resp = TestRequest::post()
-            .uri("/api/user/edit")
+        let resp = TestRequest::put()
+            .uri("/api/user/")
             .set_json(request_body)
             .insert_header(("x-authentication", "black"))
             .send_request(&app)
             .await;
-        assert_eq!(resp.status(), StatusCode::INTERNAL_SERVER_ERROR);
+        assert_eq!(resp.status(), StatusCode::BAD_REQUEST);
         let request_body = json!({
-            "username": "Iongler",
+            "username": "Iongler"
         });
 
-        let req = TestRequest::post()
-            .uri("/api/user/edit")
+        let req = TestRequest::put()
+            .uri("/api/user/")
             .set_json(request_body)
             .insert_header(("x-authentication", "black"))
             .to_request();
